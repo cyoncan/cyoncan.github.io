8d8ae984fdc076eed00f2f967b14d20be6462f24
cyoncan <cyoncan@gmail.com>
2017-07-12 18:45:18 +0800

    Site updated: 2017-07-12 18:45:18

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Tue Jul 11 2017 18:46:28 GMT+0800 (CST)
+// build time:Wed Jul 12 2017 18:44:59 GMT+0800 (CST)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
